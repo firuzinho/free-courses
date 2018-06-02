d7f4bc5c2d8c54808ea5d53f8098b305249706d0
firuzinho <firuzinhokodirov@gmail.com>
2018-06-03 03:13:02 +0700

    not yet solved task3.

---
+++ task3.js
@@ -0,0 +1,8 @@
+// Задача 3. Сложить 2 временных промежутка и вывести общую длительность в человекочитаемом виде.
+
+//Получение аргументов
+var getDay = parseInt(process.argv[2]),
+	getMonth = process.argv[3],
+	getYear = parseInt(process.argv[4]),
+	curMonthIndex,
+	numOfDays;
