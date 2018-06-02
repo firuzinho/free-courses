7cd6b2dd1517bf8ce0cb6c9e1914172712a4f5b7
firuzinho <firuzinhokodirov@gmail.com>
2018-06-03 01:38:21 +0700

    solved task task1

---
+++ task1.js
@@ -0,0 +1,13 @@
+// Задача 1. Реализовать функцию перевода числа из одной системы счисления в другую.
+
+// Получение аргументов
+var num = process.argv[2],
+	curBase = process.argv[3],
+	newBase = process.argv[4],
+	resultNum;
+
+// преобразования данного числа в системе счисления curBase в систему счисления newBase
+resultNum = Number(parseInt(num, +curBase)).toString(+newBase);
+
+// вывод
+process.stdout.write(resultNum);
